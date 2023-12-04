200a6f738032697359459b600d39672094bcc112
ThanhLoi64 <118653690+ThanhLoi64@users.noreply.github.com>
2023-12-04 07:43:31 +0700

    .

--- Data/js/main.js
+++ Data/js/main.js
@@ -2,3 +2,24 @@ function toggleMap() {
     var mapContainer = document.querySelector('.map-container');
     mapContainer.style.display = (mapContainer.style.display === 'none') ? 'block' : 'none';
   }
+
+  let arrow = document.querySelectorAll(".arrow");
+  for (var i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener("click", (e)=>{
+   let arrowParent = e.target.parentElement.parentElement;//selecting main parent of arrow
+   arrowParent.classList.toggle("showMenu");
+    });
+  }
+  let sidebar = document.querySelector(".sidebar");
+  let sidebarBtn = document.querySelector(".bx-menu");
+  console.log(sidebarBtn);
+  sidebarBtn.addEventListener("click", ()=>{
+    sidebar.classList.toggle("close");
+  });  
+  
+
+  $(".close").click(function(){
+    $('.outside').toggleClass('in');
+    $('.bar').toggleClass('active');
+    $(this).toggleClass('is-showing');
+  });
