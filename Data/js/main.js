45ef0daa39fba6063805ae6891429a1c969a9b9a
ThanhLoi64 <118653690+ThanhLoi64@users.noreply.github.com>
2023-12-17 01:49:56 +0700

    11

--- Data/js/main.js
+++
@@ -1,4 +0,0 @@
-function toggleMap() {
-    var mapContainer = document.querySelector('.map-container');
-    mapContainer.style.display = (mapContainer.style.display === 'none') ? 'block' : 'none';
-  }
